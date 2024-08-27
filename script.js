12ac0e89b5969b2e9e6c46442928247c61627fe8
unknown <el_word9@yahoo.com>
2024-08-27 11:48:30 -0500

    Huge update

---
+++ script.js
@@ -0,0 +1,8 @@
+ // Initialize AOS
+ AOS.init();
+
+    $(document).ready(function() {
+        $('#brandCarousel').carousel({
+            interval: 2000, // Change slide every 3 seconds
+        });
+    });
